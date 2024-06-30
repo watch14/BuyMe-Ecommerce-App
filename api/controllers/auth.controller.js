de83d15d9f9620a7db68356bdc3c7895ba22c863
watch14 <watchman1414@gmail.com>
2024-06-30 08:32:33 +0100

    login

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,39 @@
 import Role from "../models/Role.js";
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 //register user / create an account
 export const register = async (req, res, next) => {
   const role = await Role.find({ role: "User" });
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
   const newUser = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPassword,
     roles: role,
   });
   await newUser.save();
-  return res.status(200).send("User Registered / Created!");
+  return res.status(200).send("User Created Successfuly!");
+};
+
+//login
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send("User Email Not found!");
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).send("Wrong Password!");
+    }
+    return res.status(200).send("loged in Successfuly!");
+  } catch (error) {
+    return res.status(400).send("Login Went Wrong!");
+  }
 };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 //register
 router.post("/register", register);
+
+//login
+router.post("/login", login);
 
 export default router;
