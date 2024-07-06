da0c6244b037c52667f5c5db87ce35a97ffd0111
watch14 <watchman1414@gmail.com>
2024-07-06 12:23:46 +0100

    tailwind - firebase - ... installed

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {'path': '', component:HomeComponent},
+    // {'path': 'login', component:LoginComponent},
+    // {'path': 'register', component:RegisterComponent},
+    // {'path': 'rest-password', component:ResetPasswordComponent},
+    // {'path': 'image-uploader', component:ImageUploaderComponent},
+];
