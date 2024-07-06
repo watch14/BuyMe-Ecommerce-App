03cbcd7d5067d67e937d032a89def6f9584d931f
watch14 <watchman1414@gmail.com>
2024-07-06 12:33:09 +0100

    page + component + router

---
+++ client/src/app/components/image-uploader/image-uploader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-uploader',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-uploader.component.html',
+  styleUrl: './image-uploader.component.css'
+})
+export class ImageUploaderComponent {
+
+}
