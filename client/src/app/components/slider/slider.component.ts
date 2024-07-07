08e146564a604edb837562d05b3d2e28c9b27ad9
watch14 <watchman1414@gmail.com>
2024-07-07 20:18:03 +0100

    slider

--- client/src/app/components/slider/slider.component.ts
+++ client/src/app/components/slider/slider.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css'
 })
