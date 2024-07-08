4c79cebd0859d138e8dababefff1850fe3d48290
Iheb Chaoued <ihebchaoued@gmail.com>
2024-07-08 03:52:15 +0100

    product api

--- client/src/app/pages/product/product.component.ts
+++ client/src/app/pages/product/product.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule, RouterModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  readonly baseAPIUrl = 'http://localhost:3000/api/product/';
+  productId = '668af32a7f6d0bc641d8978f'; // Default product ID
 
+  product: any = {}; // Assuming a single product object
+
+  constructor(private http: HttpClient) {}
+
+  fetchProduct() {
+    const apiUrl = this.baseAPIUrl + this.productId;
+    this.http.get<any>(apiUrl).subscribe(
+      (response: any) => {
+        this.product = response; // Assign product object from response
+        console.log('Fetched product:', this.product);
+      },
+      (error: any) => {
+        console.error('Error fetching product:', error);
+      }
+    );
+  }
+  ngOnInit() {
+    this.fetchProduct();
+  }
 }
--- client/src/app/pages/shop/shop.component.ts
+++ client/src/app/pages/shop/shop.component.ts
@@ -7,20 +7,15 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [HttpClientModule, 
-            CommonModule, 
-            RouterModule, 
-            ],
+  imports: [HttpClientModule, CommonModule, RouterModule],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+  styleUrl: './shop.component.css',
 })
 export class ShopComponent implements OnInit {
   readonly APIUrl = 'http://localhost:3000/api/product/search?skip=0&take=16'; // Update with your API endpoint
   products: any[] = [];
 
   constructor(private http: HttpClient) {}
-
-
 
   fetchProducts() {
     this.http.get<any>(this.APIUrl).subscribe(
