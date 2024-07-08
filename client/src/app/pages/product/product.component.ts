0f80e607c180abed03e8544166514b966858c42c
Iheb Chaoued <ihebchaoued@gmail.com>
2024-07-08 18:18:59 +0100

    single product

--- client/src/app/pages/product/product.component.ts
+++ client/src/app/pages/product/product.component.ts
@@ -3,11 +3,12 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { map, Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [HttpClientModule, CommonModule, RouterModule],
+  imports: [HttpClientModule, CommonModule, RouterModule, FormsModule],
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
@@ -16,6 +17,8 @@ export class ProductComponent implements OnInit {
   productId: string | undefined;
   product: any = {};
   categoryName$: Observable<string> | undefined;
+  quantity: number = 1; // Initial quantity
+  isFavorite: boolean = false;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
@@ -43,5 +46,27 @@ export class ProductComponent implements OnInit {
   getCategoryName(categoryId: string): Observable<string> {
     const apiUrl = `${this.baseAPIUrl}category/${categoryId}`;
     return this.http.get<any>(apiUrl).pipe(map((response: any) => response.data.categoryName));
+  }
+
+  addToFavorites() {
+    if (this.isFavorite) {
+      // Remove from favorites
+      this.isFavorite = false;
+      // Perform any necessary API calls or local operations for removing from favorites
+    } else {
+      // Add to favorites
+      this.isFavorite = true;
+      // Perform any necessary API calls or local operations for adding to favorites
+    }
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
   }
 }
