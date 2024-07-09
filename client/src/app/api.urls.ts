d4a857b4da88b1389eac772eab3968e4073cf699
watch14 <watchman1414@gmail.com>
2024-07-09 16:49:57 +0100

    search bar works

--- client/src/app/api.urls.ts
+++ client/src/app/api.urls.ts
@@ -2,6 +2,6 @@ import { AuthService } from './services/auth.service';
 export const apiUrls = {
     AuthServiceApi: 'http://localhost:3000/api/auth',
 
-    ProductApi: 'http://localhost:3000/api/product/search',
-    
+    ProductApi: 'http://localhost:3000/api/product/',
+
 }
