f16419b842cefaf13f6b3dd878333873ef2a38b6
watch14 <watchman1414@gmail.com>
2024-07-27 09:30:04 +0100

    full push project fully Done ✅😁

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -11,12 +11,9 @@ import { provideHttpClient } from '@angular/common/http';
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCOO7afbVou_EoGIzMAZ0mMGftUfEz4WpY",
-  authDomain: "byme-picture.firebaseapp.com",
-  projectId: "byme-picture",
-  storageBucket: "byme-picture.appspot.com",
-  messagingSenderId: "1035349061763",
-  appId: "1:1035349061763:web:92bdd1af0f3b2fdeaa2bb6"
+//firebase info
+
+appId: "0"
 
 };
 
@@ -24,17 +21,9 @@ const firebaseConfig = {
 
 const firebaseConfig2 = {
 
-  apiKey: "AIzaSyBMHPbLIt-ePXEnSEHfhzSgEl0flz7zt0k",
+//firebase info
 
-  authDomain: "buyme-3fd57.firebaseapp.com",
-
-  projectId: "buyme-3fd57",
-
-  storageBucket: "buyme-3fd57.appspot.com",
-
-  messagingSenderId: "98610282041",
-
-  appId: "1:98610282041:web:4c8463b67f77982afed7e8"
+  appId: "0"
 
 };
 
